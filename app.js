4106bf59398dbc913950e0fcee6cc177676f404c
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-12-07 19:34:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// ./assits/CV Muhammad Ahmed.pdf
 
+AOS.init();
 
 
 let cvDownload = ()=>{
     let cvFilePath = "./assits/CV-Muhammad-Ahmed.pdf"
     let downloadLink = document.createElement('a')
+    downloadLink.setAttribute("target","_blank");
     downloadLink.href = cvFilePath
     downloadLink.download = "CV-Muhammad-Ahmed.pdf"
     downloadLink.click();
