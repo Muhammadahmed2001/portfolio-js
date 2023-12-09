01eb98000d00e57adbbf68407f920e09a4917c1d
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-12-09 20:32:13 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 
 AOS.init();
+setTimeout(function() {
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+  }, 2000);
 
+// let cvDownload = ()=>{
+//     let cvFilePath = "./assits/CV-Muhammad-Ahmed.pdf"
+//     let downloadLink = document.createElement('a')
+//     downloadLink.setAttribute("target","_blank");
+//     downloadLink.setAttribute("download","Copy of Minimalist Web Designer Resume CV");
+//     downloadLink.href = cvFilePath
+//     // downloadLink.download = "CV-Muhammad-Ahmed.pdf"
+//     downloadLink.click();
 
-let cvDownload = ()=>{
-    let cvFilePath = "./assits/CV-Muhammad-Ahmed.pdf"
-    let downloadLink = document.createElement('a')
-    downloadLink.setAttribute("target","_blank");
-    downloadLink.href = cvFilePath
-    downloadLink.download = "CV-Muhammad-Ahmed.pdf"
-    downloadLink.click();
-
-}
+// }
